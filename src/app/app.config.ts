c8ffdfbb8e56aa04f0203719584cbee76c3df447
Ciro <cirosardinha.projects@gmail.com>
2026-04-15 22:14:31 -0300

    .

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,27 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
 
 import { routes } from './app.routes';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import localePT from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+import { provideToastr } from 'ngx-toastr';
+
+registerLocaleData(localePT);
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    { provide: MAT_DATE_LOCALE, useValue: 'pt-BR' },
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideNativeDateAdapter(),
+    provideToastr({
+      timeOut: 2000,
+      positionClass: 'toast-top-right',
+      preventDuplicates: true,
+      closeButton: true,
+      tapToDismiss: true,
+    }),
+  ],
 };
